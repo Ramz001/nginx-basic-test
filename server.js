fe6dab21f89ae5ca58a15eca9e6656e9257e8cf8
Ramz001 <rkenjaev1@gmail.com>
2025-12-29 06:50:08 +0500

    feat: add Nginx configuration and update server to serve dynamic app name

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
@@ -8,8 +9,15 @@ const appName = process.env.APP_NAME
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by ${appName}`);
+    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error loading page');
+            return;
+        }
+        const html = data.replace('$$', appName);
+        res.send(html);
+        console.log(`Request served by ${appName}`);
+    });
 });
 
 app.listen(port, () => {
